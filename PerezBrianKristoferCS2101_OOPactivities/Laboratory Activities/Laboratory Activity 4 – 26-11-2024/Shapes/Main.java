09a018fb4e28e4a261300e70de9e9534384f2b60
Brian-Kristofer-Perez <161929458+Brian-Kristofer-Perez@users.noreply.github.com>
2024-11-26 22:19:48 +0800

    Update Main.java

--- PerezBrianKristoferCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 4 – 26-11-2024/Shapes/Main.java
+++ PerezBrianKristoferCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 4 – 26-11-2024/Shapes/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
        Square square = new Square(9);
        Rectangle rectangle = new Rectangle(9, 11);
-       Triangle triangle = new Triangle(2, 3, 4 );
+       Triangle triangle = new Triangle(2, 3, 4);
 
        Shape[] shapes = {square, rectangle, triangle};
        ShapeCollection shapeCollection = new ShapeCollection(shapes);
